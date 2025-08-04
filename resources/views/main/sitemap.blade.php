6963465f9acb78b96ab78b06aa53702ecd614518
Gulyaev Martinas <killfind@yandex.ru>
2025-08-04 23:43:48 +0300

    добавление ссылок для карты сайта

--- resources/views/main/sitemap.blade.php
+++ resources/views/main/sitemap.blade.php
@@ -7,58 +7,58 @@
 <div class="sitemap-items">
     <div class="sitemap-item">
         <ul>
-            <li><a href="">Главная страница</a></li>
+            <li><a href="{{ route('index') }}">Главная страница</a></li>
             <li>
-                <a href="">Новинки</a>
+                <a href="{{ route('new') }}">Новинки</a>
                 <ul>
-                    <li><a href="">Женская</a></li>
-                    <li><a href="">Мужская</a></li>
-                    <li><a href="">Унисекс</a></li>
+                    <li><a href="{{ route('new_women') }}">Женская</a></li>
+                    <li><a href="{{ route('new_men') }}">Мужская</a></li>
+                    <li><a href="{{ route('new_unisex') }}">Унисекс</a></li>
                 </ul>
             </li>
             <li>
-                <a href="">Парфюмерия</a>
+                <a href="{{ route('parfum') }}">Парфюмерия</a>
                 <ul>
-                    <li><a href="">Женская</a></li>
-                    <li><a href="">Мужская</a></li>
-                    <li><a href="">Унисекс</a></li>
+                    <li><a href="{{ route('parfum_women') }}">Женская</a></li>
+                    <li><a href="{{ route('parfum_men') }}">Мужская</a></li>
+                    <li><a href="{{ route('parfum_unisex') }}">Унисекс</a></li>
                 </ul>
             </li>
             <li>
-                <a href="">Уход</a>
+                <a href="{{ route('care') }}">Уход</a>
                 <ul>
-                    <li><a href="">Женская</a></li>
-                    <li><a href="">Мужская</a></li>
-                    <li><a href="">Унисекс</a></li>
+                    <li><a href="{{ route('care_women') }}">Женская</a></li>
+                    <li><a href="{{ route('care_men') }}">Мужская</a></li>
+                    <li><a href="{{ route('care_unisex') }}">Унисекс</a></li>
                 </ul>
             </li>
-            <li><a href="">Косметика</a></li>
+            <li><a href="{{ route('cosmetic') }}">Косметика</a></li>
             <li>
-                <a href="">Распродажа</a>
+                <a href="{{ route('sale') }}">Распродажа</a>
                 <ul>
-                    <li><a href="">Женская</a></li>
-                    <li><a href="">Мужская</a></li>
-                    <li><a href="">Унисекс</a></li>
+                    <li><a href="{{ route('care_women') }}">Женская</a></li>
+                    <li><a href="{{ route('care_men') }}">Мужская</a></li>
+                    <li><a href="{{ route('care_unisex') }}">Унисекс</a></li>
                 </ul>
             </li>
-            <li><a href="">Бренды</a></li>
-            <li><a href="">Ноты</a></li>
+            <li><a href="{{ route('brends') }}">Бренды</a></li>
+            <li><a href="{{ route('notes') }}">Ноты</a></li>
         </ul>
     </div>
     <div class="sitemap-item">
         <ul>
-            <li><a href="">Парфюмеры</a></li>
-            <li><a href="">О магазине</a></li>
-            <li><a href="">Новости</a></li>
-            <li><a href="">Доставка</a></li>
-            <li><a href="">Оплата</a></li>
-            <li><a href="">Скидки</a></li>
-            <li><a href="">Акции</a></li>
-            <li><a href="">Контакты</a></li>
-            <li><a href="">Подарки</a></li>
-            <li><a href="">График работы</a></li>
-            <li><a href="">Возврат</a></li>
-            <li><a href="">Политика конфедициальности</a></li>
+            <li><a href="{{ route('parfumers') }}">Парфюмеры</a></li>
+            <li><a href="{{ route('about') }}">О магазине</a></li>
+            <li><a href="{{ route('news') }}">Новости</a></li>
+            <li><a href="{{ route('delivery') }}">Доставка</a></li>
+            <li><a href="{{ route('payment') }}">Оплата</a></li>
+            <li><a href="{{ route('discounts') }}">Скидки</a></li>
+            <li><a href="{{ route('promo') }}">Акции</a></li>
+            <li><a href="{{ route('contacts') }}">Контакты</a></li>
+            <li><a href="{{ route('presents') }}">Подарки</a></li>
+            <li><a href="{{ route('worktime') }}">График работы</a></li>
+            <li><a href="{{ route('refund') }}">Возврат</a></li>
+            <li><a href="{{ route('rules') }}">Политика конфедициальности</a></li>
 
         </ul>
     </div>
